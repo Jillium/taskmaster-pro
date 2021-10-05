ff126fd298ab7dda80be8db4a272638a77766fd1
Jillium <86424914+Jillium@users.noreply.github.com>
2021-10-05 13:06:18 -0600

    audit dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,9 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  //check due date
+  auditTask(taskLi);
+
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -172,13 +175,22 @@ $('.list-group').on("click", "span", function() {
   //swap out elements 
   $(this).replaceWith(dateInput);
 
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      //when calendar is closed, force a "change" event on the 'dateinput'
+      $(this).trigger("change");
+    }
+  });
+
   // automatically focus on new element
   dateInput.trigger("focus");
 })
 
 
 //value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   //get current text
   var date = $(this)
   .val()
@@ -206,6 +218,9 @@ $(".list-group").on("blur", "input[type='text']", function() {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  // Pass task's <li> element into audtiTask() to check new due date
+  auditTask($(taskSpan).closest(".list-grou-item"));
 });
 
 
@@ -243,6 +258,32 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+// modal for the date picker 
+$("#modalDueDate").datepicker( {
+  minDate: 1
+});
+
+var auditTask = function(taskEl) {
+  // get date from the task element
+  var date = $(taskEl).find("span").text().trim();
+  // ensure it worked
+  
+
+  //convert to moment object at 5:00 pm
+  var time = moment(date, "L").set("hour", 17);
+  
+  //remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  //applyl new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+  
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
